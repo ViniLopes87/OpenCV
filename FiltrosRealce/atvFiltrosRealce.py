b80bdb64a0b192958592ce3db3102aab4b990877
Vinícius Lopes da Silva <71892190+ViniLopes87@users.noreply.github.com>
2022-09-15 09:46:19 -0300

    Implementando filtros de realce

---
+++ FiltrosRealce/atvFiltrosRealce.py
@@ -0,0 +1,48 @@
+import sys
+import numpy as np
+import cv2 as cv
+
+def main(argv):
+
+    window_name = ('Prewitt')
+    ddepth = cv.CV_16S
+
+    if len(argv) < 1:
+        print('Not enough parameters')
+        print('Usage:\nmorph_lines_detection.py < path_to_image >')
+        return -1
+
+    
+    # Load the image
+    src = cv.imread(argv[0], cv.IMREAD_COLOR)
+
+
+    # Check if image is loaded fine
+    if src is None:
+        print('Error opening image: ' + argv[0])
+        return -1
+
+    # Prewitt Operator
+    kernelx = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
+    kernely = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+
+    src = cv.GaussianBlur(src, (3, 3), 0)
+
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+    grad_x = cv.filter2D(gray, ddepth, kernelx)
+    grad_y = cv.filter2D(gray, ddepth, kernely)
+
+    abs_grad_x = cv.convertScaleAbs(grad_x)
+    abs_grad_y = cv.convertScaleAbs(grad_y)
+
+    grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+    cv.imshow(window_name, grad)
+    cv.waitKey(0)
+
+    return 0
+
+
+if __name__ == "_main_":
+    main(sys.argv[1:])
